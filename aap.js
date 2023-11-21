65529ffca1d4ec41589485e43bda2cc717ca5794
Anusraza09 <sanus8339@gmail.com>
2023-11-21 20:31:08 +0600

    first commit

---
+++ aap.js
@@ -0,0 +1,72 @@
+var variable1 = "variable1"
+alert(
+    variable1
+)
+var variable2 = "variable2"
+alert(
+    variable2
+)
+var variable3 = "variable3"
+alert(
+    variable3
+)
+   
+
+
+// Legal variable names
+var legalVariable1;
+var $my_1stVariable;
+var _variableName;
+var camelCaseExample;
+var thisIsALongVariableName;
+
+
+// Illegal variable names
+var stIllegalVariable;
+var illegalvariable;
+var myvariable;
+var varname;
+var myvariable;   
+// alert{
+//      stIllegalVariable
+//      illegalvariable
+//      myvariable
+//      varname
+//      myvariable
+
+// }
+
+
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("<p>Variable names can only contain letters, numbers, underscores, and dollar signs. For example $my_1stVariable</p>");
+document.write("<p>Variables must begin with a letter, underscore, or dollar sign. For example $name, _name, or name</p>");
+document.write("<p>Variable names are case-sensitive</p>");
+document.write("<p>Variable names should not be JS reserved words</p>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
